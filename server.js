24f58060ee79cad7799bf5f9a5f0e2bc6ae9a5ea
PSouza-10 <paulosouza300272@gmail.com>
2021-07-15 15:58:50 -0300

    use DB from env in production

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const path = require("path")
-const config = require("config")
-const Games = require("./routes/api/Games")
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+const Games = require("./routes/api/Games");
 
-const cors = require("cors")
-const auth = require("./routes/api/auth")
+const cors = require("cors");
+const auth = require("./routes/api/auth");
 
 //Initialize server
-const app = express()
+const app = express();
 
 //Select http bodyparser
-app.use(express.json())
+app.use(express.json());
 
 var corsOptions = {
-	origin: "http://localhost:3000",
-	headers: ["Content-Type", "Origin", "x-auth-token", "Access"],
-}
+  origin: "http://localhost:3000",
+  headers: ["Content-Type", "Origin", "x-auth-token", "Access"],
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 //Configure database
-const db = config.get("Database")
+const db = config.get("Database") || process.env.DB;
 
 //Connect to Database
 mongoose
-	.connect(db, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("MongoDb connected"))
-	.then(err => {
-		err ? console.log(err) : "No errors"
-	})
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDb connected"))
+  .then((err) => {
+    err ? console.log(err) : "No errors";
+  });
 
 //Set route handlers
 
-app.use("/api/jogos", Games)
-app.use("/api/auth", auth)
+app.use("/api/jogos", Games);
+app.use("/api/auth", auth);
 
 //Setup production assets
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"))
+  app.use(express.static("client/build"));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	})
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 //Set port number to enviroment or default
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 //Configure/test port
-app.listen(PORT, () => console.log("Started on PORT " + PORT))
+app.listen(PORT, () => console.log("Started on PORT " + PORT));
